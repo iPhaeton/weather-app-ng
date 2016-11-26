7de531927ca0b4e92a8cc9ba9294f3f479c418f5
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-11-26 09:59:34 +0300

    Add a map depiction to the map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { ProvideService } from "../provide.service";
+declare var google: any;
 
 @Component({
   selector: 'app-map',
@@ -8,12 +9,19 @@ import { ProvideService } from "../provide.service";
 })
 export class MapComponent implements OnInit {
 
-  constructor(private provide: ProvideService) {}
+  constructor(private provide: ProvideService, private elem: ElementRef) {}
 
   ngOnInit() {
     this.provide.location((err, pos) => {
       if (err) console.log(err);
       else console.log(pos);
+
+      var mapProperties = {
+        center: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(this.elem.nativeElement.querySelector("#canvas"), mapProperties);
     });
   }
 
